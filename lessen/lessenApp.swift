298cfbb9078a95c2a86754b05609018dc5b28b31
Aspyryan <jasper.baetsle@gmail.com>
2022-11-08 18:24:47 +0100

    Initial Commit

---
+++ lessen/lessenApp.swift
@@ -0,0 +1,17 @@
+//
+//  lessenApp.swift
+//  lessen
+//
+//  Created by Gast Gebruiker on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct lessenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
