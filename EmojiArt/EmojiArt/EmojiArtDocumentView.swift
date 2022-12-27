b4ab2620ad5976aef7bd0aeb0f611b6ed4578c53
Aspyryan <jasper.baetsle@gmail.com>
2022-12-27 21:03:17 +0100

    lesson 13

--- EmojiArt/EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -58,6 +58,9 @@ struct EmojiArtDocumentView: View {
                 default: break
                 }
             }
+            .onReceive(document.$backgroundImage) { image in
+                zoomToFit(image, in: geometry.size)
+            }
         }
     }
     
